bef3d4c2151d3f8a7c0003cc3a82de63eb2c67c8
Brian Green <30701130+bhgcreative@users.noreply.github.com>
2017-08-03 13:09:28 -0600

    Update GuessMeFinally.java
    removed package reference to earlier project created in NetBeans

--- GuessMeFinally.java
+++ GuessMeFinally.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.practiceprogramming6;
 
 import java.util.Random;
 import java.util.Scanner;
